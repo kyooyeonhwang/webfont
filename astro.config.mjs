97b3b48f4b994c37fa7ed24600c561ce811cb29c
DESKTOP-IA3D7FU\etribe <ky.hwang@etribe.com>
2022-02-25 16:09:56 +0900

    빌드 오류 테스트

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Enable the Preact renderer to support Preact JSX components.
-  renderers: ['@astrojs/renderer-preact'],
+  // renderers: ['@astrojs/renderer-preact'],
   // files in `static/` will be blindly copied to `public/`
   // public: 'static',
   // `public/` is where the built website will be output to
-  dist: 'public',
+  // dist: 'public',
   buildOptions: {
-    sitemap: true,
-    site: 'https://compassionate-liskov-bfec94.netlify.app/',
+    // sitemap: true,
+    site: 'http://localhost',
   },
 });
