0d6ae3962ff01695493f1d14e03a5de338c2e432
kyooyeonhwang <kyooyeon.hwang@gmail.com>
2024-09-16 12:09:37 +0900

    scss파일을 css파일로 컴파일하기 위해서 설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,14 @@
 // helpful tooltips, and warnings if your exported object is invalid.
 // You can disable this by removing "@ts-check" and `@type` comments below.
 
+// import { defineConfig } from 'astro/config';
+// import sass from '@astrojs/sass';
+
+// export default defineConfig({
+//   integrations: [sass()],
+// });
+
+
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ (
 	{
